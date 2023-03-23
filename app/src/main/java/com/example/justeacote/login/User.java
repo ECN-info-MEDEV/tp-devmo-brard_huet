e8081821e752274c5c0bc96d1ca514595d0cc6a5
Tristan <brardtristan@gmail.com>
2023-03-23 09:32:08 +0100

    Affichage Login et Register

---
+++ app/src/main/java/com/example/justeacote/login/User.java
@@ -0,0 +1,50 @@
+package com.example.justeacote.login;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity (tableName = "user")
+public class User {
+
+    @PrimaryKey (autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int id;
+    @NonNull
+    private String username;
+    @NonNull
+    private String password;
+
+    public User(){
+        id = 0;
+        username = "";
+        password = "";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(@NonNull String username) {
+        this.username = username;
+    }
+
+    @NonNull
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NonNull String password) {
+        this.password = password;
+    }
+}
